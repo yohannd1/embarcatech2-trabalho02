861da568241af6c3b9b3d327cfb9e38864e07ddd
yohannd1 <mitonanan12@gmail.com>
2025-04-23 20:32:33 -0300

    adicionando protótipo de interface serial & cálculo de cores

--- src/main.c
+++ src/main.c
@@ -23,6 +23,18 @@ const uint32_t ADC_RESOLUTION = 4095;
 
 static void on_button_press(uint gpio, uint32_t events);
 
+/**
+ * Calcula a resistência para resistores de 4 bandas, com tolerância de 5%.
+ *
+ * @param resist o valor da resistência (510 <= resistance <= 100000)
+ * @param digit1 (vai receber) a cor do primeiro dígito (0 <= *digit1 <= 9)
+ * @param digit2 (vai receber) a cor do segundo dígito (0 <= *digit2 <= 9)
+ * @param mult (vai receber) a cor do multiplicador (0 <= *mult <= 9)
+ *
+ * Retorna um valor indicando se a função rodou com sucesso.
+ */
+static bool calc_res_colors_4band(float resist, uint8_t *digit1, uint8_t *digit2, uint8_t *mult);
+
 int main(void) {
 	stdio_init_all();
 
@@ -58,60 +70,78 @@ int main(void) {
 
 	const int PASS_COUNT = 500;
 
-	bool cor = true;
+	/* bool cor = true; */
+	/* while (true) { */
+	/* 	adc_select_input(2); // Seleciona o ADC para eixo X. O pino 28 como entrada analógica */
+
+	/* 	float soma = 0.0f; */
+	/* 	for (int i = 0; i < PASS_COUNT; i++) { */
+	/* 		/1* soma += adc_read(); *1/ */
+	/* 		soma += 800; */
+	/* 		sleep_ms(1); */
+	/* 	} */
+	/* 	float media = soma / PASS_COUNT; */
+
+	/* 	// Fórmula simplificada: r_x = r_conhecido * ADC_encontrado /(ADC_RESOLUTION - adc_encontrado) */
+	/* 	float r_x = (r_conhecido * media) / (ADC_RESOLUTION - media); */
+
+	/* 	snprintf(str_x, 5, "%.0f", media); */
+	/* 	snprintf(str_y, 5, "%.0f", r_x); */
+
+	/* 	// cor = !cor; */
+	/* 	//  Atualiza o conteúdo do display com animações */
+
+	/* 	ssd1306_fill(&display, !cor); */
+	/* 	ssd1306_rect(&display, 3, 3, 122, 60, cor, !cor); */
+	/* 	ssd1306_line(&display, 3, 25, 123, 25, cor); */
+	/* 	ssd1306_line(&display, 3, 37, 123, 37, cor); */
+
+	/* 	uint8_t x, y; */
+
+	/* 	x = 8, y = 6; */
+	/* 	ssd1306_draw_string(&display, "CEPEDI   TIC37", &x, &y); */
+
+	/* 	x = 20, y = 16; */
+	/* 	ssd1306_draw_string(&display, "EMBARCATECH", &x, &y); */
+
+	/* 	x = 10, y = 28; */
+	/* 	ssd1306_draw_string(&display, "  Ohmimetro", &x, &y); */
+
+	/* 	x = 13, y = 41; */
+	/* 	ssd1306_draw_string(&display, "ADC", &x, &y); */
+
+	/* 	x = 50, y = 41; */
+	/* 	ssd1306_draw_string(&display, "Resisten.", &x, &y); */
+
+	/* 	ssd1306_line(&display, 44, 37, 44, 60, cor); */
+
+	/* 	x = 8, y = 52; */
+	/* 	ssd1306_draw_string(&display, str_x, &x, &y); */
+
+	/* 	x = 59, y = 52; */
+	/* 	ssd1306_draw_string(&display, str_y, &x, &y); */
+
+	/* 	ssd1306_send_data(&display); */
+
+	/* 	sleep_ms(700); */
+	/* } */
+
 	while (true) {
-		adc_select_input(2); // Seleciona o ADC para eixo X. O pino 28 como entrada analógica
-
-		float soma = 0.0f;
-		for (int i = 0; i < PASS_COUNT; i++) {
-			/* soma += adc_read(); */
-			soma += 800;
-			sleep_ms(1);
+		float r;
+		printf("Valor: ");
+		if (scanf("%f", &r) == 0) {
+			printf("\nValor digitado inválido!\n");
+			fflush(stdin);
+			continue;
 		}
-		float media = soma / PASS_COUNT;
 
-		// Fórmula simplificada: r_x = r_conhecido * ADC_encontrado /(ADC_RESOLUTION - adc_encontrado)
-		float r_x = (r_conhecido * media) / (ADC_RESOLUTION - media);
+		uint8_t d1, d2, m;
+		if (!calc_res_colors_4band(r, &d1, &d2, &m)) {
+			printf("Falha ao calcular o valor da resistência\n");
+			continue;
+		}
 
-		snprintf(str_x, 5, "%.0f", media);
-		snprintf(str_y, 5, "%.0f", r_x);
-
-		// cor = !cor;
-		//  Atualiza o conteúdo do display com animações
-
-		ssd1306_fill(&display, !cor);
-		ssd1306_rect(&display, 3, 3, 122, 60, cor, !cor);
-		ssd1306_line(&display, 3, 25, 123, 25, cor);
-		ssd1306_line(&display, 3, 37, 123, 37, cor);
-
-		uint8_t x, y;
-
-		x = 8, y = 6;
-		ssd1306_draw_string(&display, "CEPEDI   TIC37", &x, &y);
-
-		x = 20, y = 16;
-		ssd1306_draw_string(&display, "EMBARCATECH", &x, &y);
-
-		x = 10, y = 28;
-		ssd1306_draw_string(&display, "  Ohmimetro", &x, &y);
-
-		x = 13, y = 41;
-		ssd1306_draw_string(&display, "ADC", &x, &y);
-
-		x = 50, y = 41;
-		ssd1306_draw_string(&display, "Resisten.", &x, &y);
-
-		ssd1306_line(&display, 44, 37, 44, 60, cor);
-
-		x = 8, y = 52;
-		ssd1306_draw_string(&display, str_x, &x, &y);
-
-		x = 59, y = 52;
-		ssd1306_draw_string(&display, str_y, &x, &y);
-
-		ssd1306_send_data(&display);
-
-		sleep_ms(700);
+		printf("R = %.f OHM; { %u %u %u }\n", r, d1, d2, m);
 	}
 }
 
@@ -119,4 +149,8 @@ static void on_button_press(uint gpio, uint32_t events) {
 	if (gpio == BUTTON_B_PIN) {
 		reset_usb_boot(0, 0);
 	}
+}
+
+static bool calc_res_colors_4band(float resist, uint8_t *digit1, uint8_t *digit2, uint8_t *mult) {
+	return false;
 }
